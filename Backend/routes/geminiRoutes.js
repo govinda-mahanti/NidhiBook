f1aaffb16c7e5d5d4b541940e4bd2d46db395aa8
govinda-mahanti <gmahanti955@gmail.com>
2026-06-21 00:37:57 +0530

    add

--- Backend/routes/geminiRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { generateGeminiResponse } from "../controllers/geminiControllers.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-router.get("/suggestions", authMiddleware, generateGeminiResponse);
-
-export default router;
