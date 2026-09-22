a23a03d1274b2bdbef58b895e5ac05f2e74f6b2e
govinda-mahanti <gmahanti955@gmail.com>
2026-09-23 00:09:24 +0530

    check jenkins

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -705,6 +705,7 @@ const HeroSection = () => {
         <div className="absolute top-1/4 left-0 w-96 h-96 bg-purple-600/10 rounded-full blur-3xl"></div>
         <div className="absolute bottom-1/4 right-0 w-96 h-96 bg-cyan-600/10 rounded-full blur-3xl"></div>
       </section>
+      <h1>me hun don</h1>
     </>
   );
 };
